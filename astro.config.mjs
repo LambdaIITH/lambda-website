c4ea2b80c2b8556e9f11c460507826b1d902a6fa
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2025-01-04 21:37:02 +0530

    !(bug-fix): solid js now in root of components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,18 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
-  // renderers: ["@astrojs/renderer-react"],
+  renderers: ['@astrojs/renderer-react'],
   integrations: [
     mdx(),
     sitemap(),
+    solidJs(),
+    tailwind({ applyBaseStyles: false }),
     react({
-      // experimentalReactChildren: true,
+      experimentalReactChildren: true,
       include: ["**/react/*"],
     }),
-    solidJs({
-      include: ["**/solid/*"],
-    }),
-    tailwind({ applyBaseStyles: false }),
   ],
 });
